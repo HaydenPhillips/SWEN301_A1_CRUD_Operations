8025aa27a4e114c4b8797479fdd2188d0a86706a
Hayden Phillips <hayden.colony@gmail.com>
2019-03-25 01:38:04 +1300

    add Create add Read add Update add Delete add getAllStudents add getAllDegrees

--- src/test/java/test/nz/ac/vuw/swen301/assignment1/StudentManagerTest.java
+++ src/test/java/test/nz/ac/vuw/swen301/assignment1/StudentManagerTest.java
@@ -1,11 +1,16 @@
 package test.nz.ac.vuw.swen301.assignment1;
 
+import nz.ac.vuw.swen301.assignment1.Degree;
 import nz.ac.vuw.swen301.assignment1.Student;
 import nz.ac.vuw.swen301.assignment1.StudentManager;
 import nz.ac.vuw.swen301.studentmemdb.StudentDB;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
 
 public class StudentManagerTest {
 
@@ -20,8 +25,122 @@ public class StudentManagerTest {
 
     @Test
     public void dummyTest() throws Exception {
-//        Student student = new StudentManager().findById("id42");
-        // THIS WILL INITIALLY FAIL
-//        assertNotNull(student);
+        Student s = StudentManager.readStudent("id420");
+        assert s != null;
+        s.setFirstName("Scotty");
+        StudentManager.update(s);
+        assertTrue(StudentManager.readStudent("id420").getFirstName().equals("Scotty"));
+
+    }
+
+    @Test
+    public void truncationErrorTest() throws Exception {
+        Student s = StudentManager.readStudent("id420");
+        assert s != null;
+        StudentManager.update(s);
+        String sFirstName = s.getFirstName();
+        assertTrue(sFirstName.length() <= 10);
+    }
+
+
+    @Test
+    public void testReadStudent() throws Exception {
+        Student student = new StudentManager().readStudent("id420");
+
+        assert student != null;
+        System.out.println(student.getDegreeName());
+        System.out.println(student.getDegreeID());
+        Degree dg = StudentManager.readDegree("id20");
+        System.out.println(dg.getName()+" "+dg.getId());
+        System.out.println("Degree: "+dg.getId()+" - "+dg.getName()+" - "+dg+"");
+        assertNull(student.getDegreeName().equals("fc"));
+    }
+
+
+    @Test
+    public void testReadDegree() throws Exception {
+        Degree deg = StudentManager.readDegree("deg1");
+        assert deg != null;
+        assertTrue(deg.getName().equals("BSc Computer Graphics"));
+    }
+
+    @Test
+    public void testAllStudentID() throws Exception {
+        Collection<String> allStudents = StudentManager.getAllStudentIds();
+        assert allStudents != null;
+        int count = 0;
+        for (String s : allStudents) {
+            assertEquals(s,("id"+(count++)));
+        }
+    }
+
+//    @Test
+//    public void testDeleteStudent() throws Exception {
+//        Student s = StudentManager.readStudent("id69");
+//        StudentManager.delete(s);
+//        assertNull(StudentManager.readStudent("id69"));
+//    }
+
+
+    @Test
+    public void testAllDegreeIDs01() throws Exception {
+        ArrayList<String> allDegrees = (ArrayList<String>) StudentManager.getAllDegreeIds();
+        assert allDegrees != null;
+        int count = 0;
+        for (String s : allDegrees) {
+            assertTrue(s.equals("deg"+(count++)));
+        }
+    }
+
+    @Test
+    public void testAllDegreeIDs02() throws Exception {
+        ArrayList<String> allDegrees = (ArrayList<String>) StudentManager.getAllDegreeIds();
+        assert allDegrees != null;
+        assertTrue(allDegrees.size() == 16);
+    }
+
+    @Test
+    public void testUpdateStudent(){
+        Student originalStudent = StudentManager.readStudent("id69");
+        assert originalStudent!= null;
+        originalStudent.setFirstName("Scotty");
+        StudentManager.update(originalStudent);
+        Student changedStudent = StudentManager.readStudent("id69");
+        assert changedStudent != null;
+        assertTrue(changedStudent.getFirstName().equals("Scotty"));
+    }
+
+    @Test
+    public void testCreate01(){
+        Degree degree = StudentManager.readDegree("deg4");
+        assert degree != null;
+        StudentManager.createStudent("DoWang","Scotty", degree);
+        assertNotNull(StudentManager.readStudent("id10000"));
+    }
+
+
+//    @Test
+//    public void testCreate02(){
+//        Degree degree = StudentManager.readDegree("deg1");
+//        assert degree != null;
+//        for (int i = 0; i < 100; i++) {
+//            StudentManager.createStudent("Do"+i,"Scott"+i, degree);
+//        }
+//        for (int i = 0; i < 100; i++) {
+//            assertNotNull(StudentManager.readStudent("id"+numberOfRows++));
+//        }
+//    }
+
+
+    @Test
+    public void test1000read(){
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            StudentManager.readStudent("id"+ (Math.random() * 10000));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("time taken: " + (end-start));
+
+        assertTrue(end-start < 1000);
     }
 }
